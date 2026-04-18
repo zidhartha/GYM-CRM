0dd90757d19a7f7f131c56de41930cdca65e83ed
Davit Jincharadze <datojincharadze26@gmail.com>
2026-04-19 01:26:02 +0400

    replaced hard coded jwk-uri with eureka service discovery

--- gym-crm-workload/src/main/java/com/gym/crm/config/SecurityConfig.java
+++ gym-crm-workload/src/main/java/com/gym/crm/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.gym.crm.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,11 +10,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwksUri;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RestTemplate restTemplate) {
+        return NimbusJwtDecoder.withJwkSetUri(jwksUri)
+                .restOperations(restTemplate)
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
